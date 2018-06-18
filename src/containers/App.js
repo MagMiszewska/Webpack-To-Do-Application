07a5e13a43034ad2d8f5816daf3b78800f570291
Magdalena Miszewska <mag.miszewska@gmail.com>
2018-06-18 16:37:48 +0200

    Add title component

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,30 +1,31 @@
 import React from 'react';
 import uuid from 'uuid';
 import style from './App.css';
+import Title from '../components/Title';
 
 class App extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
-            data: []
+            data: ['1']
         };
     }
-    addTodo(val){
+    addTodo(val) {
         const todo = {
             text: val,
             id: uuid.v4(),
         };
         const data = [...this.state.data, todo];
-        this.setState({data});
+        this.setState({ data });
     }
     removeTodo(id) {
         const remainder = this.state.data.filter(todo => todo.id !== id);
-        this.setState({data: remainder});
+        this.setState({ data: remainder });
     }
     render() {
         return (
-            <div className={style.TodoApp}>
-                Tutaj pojawią się komponenty naszej aplikacji.
+            <div>
+                <Title title='ToDo:' numberoftasks={this.state.data.length} />
             </div>
         );
     }
