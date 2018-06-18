4129ac88798e9090cf07c86a2c57afed71d5079a
Magdalena Miszewska <mag.miszewska@gmail.com>
2018-06-18 15:44:07 +0200

    Change folders and files structure, add scripts for App.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/app.js',
-        output: {
+    entry: './src/index.js',
+    output: {
         path: path.resolve(__dirname, 'build'),
         filename: 'app.bundle.js'
     },
@@ -11,6 +11,18 @@ module.exports = {
             {
                 test: /\.js$/,
                 loader: "babel-loader"
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    { loader: 'style-loader' },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true
+                        }
+                    }
+                ]
             }
         ]
     }
